9ceeb3ca3bf5047b979257c93d07a3cbd089290c
zain azhar <142168421+ZAINAZHAR303@users.noreply.github.com>
2025-01-26 18:49:11 +0500

    add images

--- src/pages/about/AboutUs.jsx
+++ src/pages/about/AboutUs.jsx
@@ -8,10 +8,10 @@ export const AboutUs = () => {
     <div className="about-page">
       <div className="about-container">
         <div className="card">
-          <img src="zubair.jpeg" alt="Zubair" className="card-img" />
+          <img src="loyd.webp" alt="loyd" className="card-img" />
           <div className="card-content">
-            <h2>M Zubair Shehzad</h2>
-            <p><h2>Backend Developer</h2></p>
+            <h2>Loyed Kirimi</h2>
+            <p><h2>Ai/Backend Developer</h2></p>
           </div>
         </div>
 
@@ -19,15 +19,15 @@ export const AboutUs = () => {
           <img src="zain.jpeg" alt="Zain" className="card-img" />
           <div className="card-content">
             <h2>Zain Azhar</h2>
-            <p> <h2>AI chatbot Developer <br />Data Scrapper</h2></p>
+            <p> <h2>Software Engineer <br />Frontend Developer</h2></p>
           </div>
         </div>
 
         <div className="card">
-          <img src="Haseeb.jpeg" alt="Haseeb" className="card-img" />
+          <img src="nerd.webp" alt="nerd" className="card-img" />
           <div className="card-content">
-            <h2>M Haseeb Younas</h2>
-            <p><h2>Frontend Developer</h2></p>
+            <h2>Tinos Noé ANAGO</h2>
+            <p><h2>CEO and Data scientist</h2></p>
           </div>
         </div>
       </div>
